ac965a14e1d945e8e3c5ec5a58ca4f84b4d86e80
Janice <janiceyang0207@gmail.com>
2019-01-18 20:31:58 -0500

    updated and rerouted home page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,15 @@
 import React from "react";
-import "../app.css";
+import "../css/app.css";
 import Route from "react-router-dom/es/Route";
 import Switch from "react-router-dom/es/Switch"
-import Root from "./pages/Root"
+import Home from "./pages/Home"
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Switch>
-          <Route exact path="/" component={Root} />
+          <Route exact path="/" component={Home} />
         </Switch>
       </div>
     )
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,15 +1,17 @@
 import React from "react";
-import "../css/home.css";
-import Link from "react-router-dom/es/Link";
+import "../../css/home.css";
+import "../../css/app.css"
 
 export default class Home extends React.Component {
     render() {
-        <div className ="center">
-            <div className="title">Journal</div>
-            <div className="subtitle">Question preview</div>
-            <div className="enter">
-                <a className="login" href="/auth/google">Login</a>
+        return (
+            <div className="center"> 
+                <div className="title">Journal</div>
+                <div className="subtitle">Question preview</div>
+                <div className="enter">
+                    <a className="login" href="/auth/google">Login</a>
+                </div>
             </div>
-        </div>
+        )
     }
 }
