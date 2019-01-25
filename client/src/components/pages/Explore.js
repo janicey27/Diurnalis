1dd6ef635a3b9271f013758b0dc3eefc8a8eb38d
Justin Park <justin.s.park77@gmail.com>
2019-01-24 19:38:52 -0500

    star rendering and socket fixed

--- client/src/components/pages/Explore.js
+++ client/src/components/pages/Explore.js
@@ -30,10 +30,11 @@ export default class Explore extends React.Component{
     }
 
     generateStars = () => {
+        const starArr = [];
         let response, i;
         for (let i = 0; i < this.props.exploreResponses.length; i++) {
             response = this.props.exploreResponses[i];
-            this.state.stars.push(<Star 
+            starArr.push(<Star 
                 key={i}
                 top={String(Math.random()*96+2)+'vh'} 
                 left={String(Math.random()*96+2)+'vw'}
@@ -41,22 +42,29 @@ export default class Explore extends React.Component{
                 content={response.content}
                 upvotes={response.upvotes}
             />);
-            console.log(this.state.stars);
         }
+        this.setState({
+            stars: starArr
+        });
+        console.log(this.state.stars);
     }
 
     initializeSocket = () => {
         this.socket = io("http://localhost:3000");
         this.socket.on("post", (response) => {
             console.log("Hello world! heh");
-            this.state.stars.push(<Star 
-                key={this.state.stars.length}
-                top={String(Math.random()*96+2)+'vh'} 
-                left={String(Math.random()*96+2)+'vw'}
-                size={String(Math.random()*15+10)+'px'} // to be updated based on like data
-                content={response.content}
-                upvotes={response.upvotes}
-            />);
+            this.setState({
+                stars: this.state.stars.concat(
+                    <Star 
+                        key={this.state.stars.length}
+                        top={String(Math.random()*96+2)+'vh'} 
+                        left={String(Math.random()*96+2)+'vw'}
+                        size={String(Math.random()*15+10)+'px'} // to be updated based on like data
+                        content={response.content}
+                        upvotes={response.upvotes}
+                    />
+                )
+            });
         });
     }
 }
