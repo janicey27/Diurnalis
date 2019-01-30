217faafe6d99c8c86d83a5293abfdf899312d924
Justin Park <justin.s.park77@gmail.com>
2019-01-30 02:19:44 -0500

    monthline fix

--- client/src/components/modules/Monthline.js
+++ client/src/components/modules/Monthline.js
@@ -5,116 +5,100 @@ import DayEntry from "./DayEntry"
 import AnimateOnChange from 'react-animate-on-change'
 
 class Monthline extends React.Component {
-  
-    
 
     constructor(props) {
         super(props);
         
         this.state = {
-            selectedMonth: "",
-            monthLength: 0,
             dayEntries: [],
             showEntry: false,
             selectedDay: -1,
             responseArray: [],
             monthUpdate: false,
         }
-
-    
     }
 
-    // setStates = (month, length, array) => {
-    //     this.setState({selectedMonth: month, monthLength: length, responseArray: array});
-    // }
-    // setMonthLength = (inputMonth) => {
-    //     console.log("hi")
-    //     if (inputMonth == "jan" ||inputMonth ==  "mar" ||inputMonth ==  "may" || inputMonth == "jul" || inputMonth == "aug" ||inputMonth ==  "oct" || inputMonth == "dec") {
-    //         this.setState({monthLength: 31})
-    //     }
-    //     else if (inputMonth == "feb") {
-    //         this.setState({monthLength: 28})
-    //     }
-    //     else {
-    //         this.setState({monthLength: 30})
-    //     }
-    // }
+    componentDidMount() {
+        this.initializeSocket();
+    }
+
+    // initialize socket
+    initializeSocket = () => {
+        // listen for post, update "active" green dot state
+        this.props.socket.on("post", (response) => {
+            if (response.creatorID === this.props.userInfo._id) {
+                if (response.month === this.props.selectedMonth) {
+                    const activityArray = this.state.dayEntries;
+                    activityArray[response.day-1] = "active";
+                    this.setState({ dayEntries: activityArray });
+                }
+            }
+        });
+    }
 
     setDayEntries = () => {
-        var activityArray = []
-        var i;
-        // console.log(this.props.responseArray)
+        const activityArray = []
+        let i;
         for (i=0; i<this.props.monthLength; i++) { //initiates dayEntries with length of the selected month
             activityArray.push("inactive");
-
         }
         
         const responses = this.props.responseArray;
-        var j;
+        let j, activeIndex;
         for (j=0; j<responses.length; j++) { //iterates through responseArray to find days with entries
             if (responses[j][0]===this.props.selectedMonth) {
-                var activeIndex = responses[j][1]-1; 
+                activeIndex = responses[j][1]-1; 
                 activityArray[activeIndex] = "active";
-            }   
-            
+            }
         }
         this.setState({dayEntries: activityArray});
-        
     }
     
-
     componentDidUpdate(prevProps) {
-        if (this.props.selectedMonth != prevProps.selectedMonth) {
-            // this.setStates(this.props.selectedMonth, this.props.monthLength, this.props.responseArray);
+        if (this.props.selectedMonth !== prevProps.selectedMonth) {
             this.setDayEntries();
             this.setState({monthUpdate: true});
         }
     }
 
     entryFunction = (setDay) => {
-        // console.log("click registered !");
-        // this.setState(prevState => ({
-        //     showEntry: !prevState.showEntry
-        //   }));
         this.setState({selectedDay: setDay});
         this.setState({monthUpdate: false});
-      };
+    };
   
     render() {
-
-    return (
-        <div>
-            <div className = "entry-container">
-                 <AnimateOnChange
-                baseClassName="hi"
-                animationClassName="text-enter"
-                animate={true}>
-                    <DayEntry dayIndex={this.state.selectedDay} selectedMonth={this.props.selectedMonth} questionArray={this.props.questionArray} responseArray={this.props.responseArray}/>
-                 </AnimateOnChange>
-            </div>
-        
-            <section className="timeline">
-                <ol>
+        return (
+            <div>
+                <div className = "entry-container">
                     <AnimateOnChange
-                    baseClassName="hii"
-                    animationClassName="fade-enter"
-                    animate={this.state.monthUpdate}>
-                        {Array.from(Array(this.props.monthLength).keys()).map(y => (
-                            <Day
-                                key={y}
-                                dayIndex = {y}
-                                activity={this.state.dayEntries[y]}
-                                entryFunction={this.entryFunction}
-                            />
-                        ))} 
+                    baseClassName="hi"
+                    animationClassName="text-enter"
+                    animate={true}>
+                        <DayEntry dayIndex={this.state.selectedDay} selectedMonth={this.props.selectedMonth} questionArray={this.props.questionArray} responseArray={this.props.responseArray}/>
                     </AnimateOnChange>
-                </ol>
-            </section>
+                </div>
+            
+                <section className="timeline">
+                    <ol>
+                        <AnimateOnChange
+                        baseClassName="hii"
+                        animationClassName="fade-enter"
+                        animate={this.state.monthUpdate}>
+                            {Array.from(Array(this.props.monthLength).keys()).map(y => (
+                                <Day
+                                    key={y}
+                                    dayIndex = {y}
+                                    activity={this.state.dayEntries[y]}
+                                    entryFunction={this.entryFunction}
+                                />
+                            ))} 
+                        </AnimateOnChange>
+                    </ol>
+                </section>
 
-        </div>
-    
-    );
-  }
+            </div>
+        );
+    }
 }
 
 export default Monthline;
